e21b325a308beff7cc8b627d3e9099f2b95e45a2
Daniel Brunner <0xFEEDC0DE64@gmail.com>
2018-10-15 20:48:41 +0200

    Added translations

---
+++ translations/dbguilib_en.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>QStringListWidget</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
